6147b6d36c795a7cd5e26f61853bfd0290b0545e
Renato Ribeiro <re95.renato@gmail.com>
2017-09-19 01:09:20 -0300

    Apanhando do bsearch ;/

--- ortografia.c
+++ ortografia.c
@@ -4,52 +4,65 @@
 
 #define SIZE 100
 
-unsigned char c;
+unsigned char c; // se usar apenas char, nao identifica letras acentuadas :(
 
 int ehletra(){
 	return( ((c>64)&&(c<91)) || //igual de maiusculas ou
 	 		((c>96)&&(c<123)) || //igual de minusculas ou
-			((c>191)&&(c<256))	);	//igual de acentuados
+			((c>191)&&(c<256))	);	//igual de acentuados //função que identifica se o charactere lido é uma letra
 }
 
 int ehsimbolo(){
 	return( ((c<65)||(c>90)) && //diferente de maiusculas e
 			((c<97)||(c>122)) && //diferente de minusculas e
-			((c<192)||(c>255)) ); //diferente de acentuados
+			((c<192)||(c>255)) ); //diferente de acentuados //função que identifica se o charactere lido é um simbolo
 }
+
 
 int main()
 {
+	FILE *dicionario;
+	dicionario = fopen ("/usr/share/dict/brazilian", "r") ;
+	// if(!dicionario) printf("Arquivo nao aberto\n"); else{ printf("Arquivo aberto\n");} //saber se dicionario foi aberto
 
-	char *endereco ;
-	endereco = calloc (SIZE, sizeof(char)) ;
 
-
-
+	char palavra[50];
+	int numElem=0;
+	c=getc(dicionario);
+	while((c!=255)&&(c!=-1)) { //calcula numero de caracteres do dicionario
+		numElem++;
+		c=getc(dicionario);
+	}
 
 	c = getchar();
-	int i=0;
-	int sair=0;
-	/*while (c != 255)*/while(sair==0) //enquanto c for diferente de EOF (usar EOF não esta permitindo sair do while :/)
+	while ((c != 255) && (c != -1) ) //enquanto c for diferente de EOF (usar EOF não esta permitindo sair do while :/)
 	{
 
-
-		if (ehletra())
-		{
-			endereco[i]=c;
-			
-		}else
-		{ 
-			endereco[i]='\0';
-			sair=1;
+		while((ehsimbolo()) && ((c != 255) && (c != -1))){ //enquanto o charactere lido for simbolo vai imprimindo
+			putchar(c);
+			c=getchar();
 		}
-		i++;
 
-
-
-	
-		c = getchar();
+		int i=0; 
+		while((ehletra()) && (c!=255)){ //quando ler uma letra vai adicionando ela no vetor palavra até encontrar um simbolo
+			palavra[i]=c;
+			i++;
+			c=getchar();
+		}
+		
+		palavra[i]='\0'; //quando encontrar um simbolo sinaliza o final da palavra
+		
+		if(1/*existe*/) printf("%s",palavra ); else printf("[%s]",palavra );
 	}
-	printf("[%s]",endereco );
+		
+
+/*	char strvalues[][20] = {"verme","example","strings","here"};
+	qsort (strvalues, 4, 20, (int(*)(const void*,const void*)) strcmp);
+	char *existe;
+	char palavra[20]= "verme";
+	existe = (char*)bsearch( palavra,strvalues,4,20,(int(*)(const void*,const void*)) strcmp);
+	printf("%p\n",existe);
+	printf("%s\n",existe);
+*/
 	return 0;
 }
